b53f4f477406bd3bf8a9fcdc8e3ed5a903de3f76
lrf <xlrf@secomea.com>
2024-01-25 11:06:42 +0100

    '

--- app/page.tsx
+++ app/page.tsx
@@ -6,19 +6,19 @@ export default function Home() {
       <h2 className="text-2xl font-bold text-blue-600 dark:text-amber-500">Welcome to my Next.js Blog</h2>
       <p className="mt-2 leading-relaxed  mb-10">
       
-      Welcome to my blog! I'm thrilled to share with you the journey of 
+      Welcome to my blog! Im thrilled to share with you the journey of 
       creating this platform using Next.js 14 and Tailwind CSS. This blog 
       is built to handle markdown files, allowing me to present my thoughts 
       and ideas in a structured and readable format. Next.js 14 provides a robust 
-      framework for developing server-side rendered (SSR) and static websites. 
+      framework for developing server-side rendered SSR and static websites. 
       It offers features such as automatic routing, 
-      API routes, and optimized performance out of the box. With Next.js, I've been
+      API routes, and optimized performance out of the box. With Next.js, Ive been
        able to create a seamless browsing experience, ensuring that your reading time 
        is smooth and enjoyable 1.
        <h3 className="text-xl font-bold mt-10 text-blue-600 dark:text-amber-500">  Some Other Heading</h3>
-      On the other hand, Tailwind CSS is a utility-first CSS framework that I've 
+      On the other hand, Tailwind CSS is a utility-first CSS framework that Ive 
       leveraged to style this blog. It provides low-level utility classes that allow 
-      me to build custom designs without leaving my HTML 3. Tailwind's utility classes 
+      me to build custom designs without leaving my HTML 3. Tailwinds utility classes 
       have made it incredibly easy to create a clean, modern, and responsive design for
        this blog.
 
